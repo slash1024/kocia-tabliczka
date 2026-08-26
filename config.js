0aeddd04d7d54f53745aee4517a38e87a651c467
slash1024 <57773761+slash1024@users.noreply.github.com>
2026-08-26 17:18:06 +0200

    Update config.js

--- config.js
+++ config.js
@@ -1,4 +1,3 @@
-/* Konfiguracja synchronizacji Kocia Tabliczka */
 window.KOCIA_CONFIG = {
   SUPABASE_URL: "https://zivkqtipdozvmaaudszq.supabase.co",
   SUPABASE_ANON_KEY: "sb_publishable_uct5KVRs4kGcoO2ia2-Ciw_pxbgBDe0"
